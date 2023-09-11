4530d0a2beb2beaba18993e36a90703d3681f64c
dr-robertk <dr-robertk@posteo.org>
2023-09-11 10:36:28 +0200

    Added in a test file.

---
+++ test_sample.py
@@ -0,0 +1,12 @@
+import pytest
+
+# content of test_sample.py (from pytest docu)
+def func(x):
+    return x + 1
+
+def test_answer():
+    assert func(4) == 5
+
+@pytest.mark.parametrize("test_input, expected", [(4, 5), (3, 4), (7, 8)])
+def test_func(test_input, expected):
+    assert func(test_input) == expected
